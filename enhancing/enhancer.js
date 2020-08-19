b4120aa481e4283cb4d16723e7b494f65b7884c0
christiano39 <carneson39@gmail.com>
2020-08-19 13:34:09 -0600

    completed mvp tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,34 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  return item.enhancement === 20
+    ? { ...item }
+    : { ...item, enhancement: item.enhancement + 1 };
 }
 
 function fail(item) {
-  return { ...item };
+  let durability = item.durability;
+  let enhancement = item.enhancement;
+
+  if (enhancement < 15 && durability <= 5) {
+    durability = 0;
+  } else if (enhancement < 15 && durability > 5) {
+    durability -= 5;
+  } else if (enhancement >= 15 && durability <= 10) {
+    durability = 0;
+  } else {
+    durability -= 10;
+  }
+
+  if (enhancement > 16) {
+    enhancement--;
+  }
+
+  return { ...item, durability, enhancement };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
